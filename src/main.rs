a29ae1769e2d5e70cfc67785e676e90347ebcfd4
fuyu-myk <lionelcheongzk@outlook.com>
2025-07-31 00:04:00 +0800

    Added support for reading .snow input files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::Write};
+use std::{env, fs::File, io::Write};
 
 use crate::{compilation_unit::CompilationUnit, ir::{hir::{HIRBuilder, HIRWriter}, mir::{optimisations::Optimiser, MIRBuilder, MIRWriter}}};
 
@@ -14,27 +14,34 @@ mod ir;
 
 
 fn main() -> Result<()> {
-    let input = "
-        fx main() -> int {
-            let a = 10;
-
-            while a > 0 {
-                a = a - 1;
-            }
-
-            return a;
-        }
-    ";
+    // Get command line arguments
+    let args: Vec<String> = env::args().collect();
+    
+    // Check if a file path was provided
+    if args.len() != 2 {
+        eprintln!("Usage: {} <file.snow>", args[0]);
+        return Err(anyhow!("Please provide a .snow file to compile"));
+    }
+    
+    let file_path = &args[1];
+    
+    // Check if the file has the correct extension
+    if !file_path.ends_with(".snow") {
+        return Err(anyhow!("File must have .snow extension"));
+    }
+    
+    // Read the input file
+    let input = std::fs::read_to_string(file_path)
+        .map_err(|e| anyhow!("Failed to read file '{}': {}", file_path, e))?;
 
     // Compile the input code ^0^
-    let mut compilation_unit = CompilationUnit::compile(input).map_err(|_err| anyhow!("Compilation failed"))?;
+    let mut compilation_unit = CompilationUnit::compile(&input).map_err(|_err| anyhow!("Compilation failed"))?;
     compilation_unit.run_compiler();
 
-    /* // GCC codegen
-    let program = codegen::c::CProgram::from_compilation_unit(&compilation_unit);
-    let c_return_value = program.run()?;
-    println!("C program returned {}", c_return_value);
-    */
+     // GCC codegen
+    //let program = codegen::c::CProgram::from_compilation_unit(&compilation_unit);
+    //let c_return_value = program.run()?;
+    //println!("C program returned {}", c_return_value);
 
     // HIR
     let hir_builder = HIRBuilder::new();
@@ -59,8 +66,12 @@ fn main() -> Result<()> {
     let mut optimiser = Optimiser::new();
     optimiser.optimise(&mut mir);
     let mut optimised_mir_graphviz = String::new();
+    //let mut optimised_mir_output = String::new();
+
     MIRWriter::write_graphviz(&mut optimised_mir_graphviz, &mir)?;
     File::create("optimised-mir.dot")?.write_all(optimised_mir_graphviz.as_bytes())?;
+    //MIRWriter::write_txt(&mut optimised_mir_output, &mir)?;
+    //println!("{}", optimised_mir_output);
 
     // LIR (todo)
 
